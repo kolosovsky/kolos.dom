2163e4f1a5de34d0fa6ee4b2cd3f4a1c9fa458a1
Ed Kolosovskiy <3winner3@gmail.com>
2018-02-28 12:54:07 +0300

    add browser detecting constants

--- src/dom.service.ts
+++ src/dom.service.ts
@@ -1,4 +1,7 @@
 export class DOMService {
+  IS_MAC = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+  IS_IOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !(window as any).MSStream;
+  IS_TOUCH_DEVICE = ('ontouchstart' in window || navigator.maxTouchPoints);
 
-
+  constructor() {}
 }
