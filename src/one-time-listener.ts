6bd4d83c520556ff2a154d1fe49a752ff6f3f47f
Ed Kolosovsky <ed.kolosovsky@gmail.com>
2018-07-17 17:55:42 +0300

    add toPromise method to OneTimeListener

--- src/one-time-listener.ts
+++ src/one-time-listener.ts
@@ -1,7 +1,19 @@
 import { Listener } from "./listener";
 
 export class OneTimeListener extends Listener {
+	promiseResolver: Function;
+
 	handlerCallback() {
 		this.unbind();
+
+		if (this.promiseResolver) {
+			this.promiseResolver();
+		}
+	}
+
+	toPromise() {
+		return new Promise((resolve, reject) => {
+			this.promiseResolver = resolve;
+		});
 	}
 }
