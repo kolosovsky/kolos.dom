9df3d014f728fdfe111c347a868b863ced235bbb
Ed Kolosovsky <ed.kolosovsky@gmail.com>
2018-10-30 11:58:28 +0300

    minor

--- src/dom.service.ts
+++ src/dom.service.ts
@@ -414,4 +414,12 @@ export class DOMService {
 	matches(el, selector) {
 		return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
 	}
+
+	isRetinaDisplay() {
+		if (window.matchMedia) {
+			let  mq = window.matchMedia("only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen  and (min-device-pixel-ratio: 1.3), only screen and (min-resolution: 1.3dppx)");
+
+			return (mq && mq.matches || (window.devicePixelRatio > 1));
+		}
+	}
 }
