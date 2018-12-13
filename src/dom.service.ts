5422648f9ff42decbe135d22780aef038231b486
Ed Kolosovsky <ed.kolosovsky@gmail.com>
2018-12-13 15:22:11 +0300

    fixes

--- src/dom.service.ts
+++ src/dom.service.ts
@@ -443,4 +443,29 @@ export class DOMService {
 			return (mq && mq.matches || (window.devicePixelRatio > 1));
 		}
 	}
+
+	// https://stackoverflow.com/a/979995/5385623
+	parseQueryString(query): any {
+		let vars = query.split("&");
+		let query_string = {};
+
+		for (let i = 0; i < vars.length; i++) {
+			let pair = vars[i].split("=");
+			let key = decodeURIComponent(pair[0]);
+			let value = decodeURIComponent(pair[1]);
+			// If first entry with this name
+			if (typeof query_string[key] === "undefined") {
+				query_string[key] = decodeURIComponent(value);
+				// If second entry with this name
+			} else if (typeof query_string[key] === "string") {
+				let arr = [query_string[key], decodeURIComponent(value)];
+				query_string[key] = arr;
+				// If third or later entry with this name
+			} else {
+				query_string[key].push(decodeURIComponent(value));
+			}
+		}
+
+		return query_string;
+	}
 }
