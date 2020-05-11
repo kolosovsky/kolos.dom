361d4492354c8a08c1aec32aa6fd19636987bf9a
Ed Kolosovsky <ed.kolosovsky@gmail.com>
2020-05-11 12:12:56 +0300

    introduce Mode class

--- src/one-time-listener.ts
+++ src/one-time-listener.ts
@@ -1,6 +1,10 @@
 import { Listener } from "./listener";
 
 export class OneTimeListener extends Listener {
+	static race(listeners: OneTimeListener[]) {
+		return Promise.race(listeners.map(listener => listener.toPromise()));
+	}
+
 	promiseResolver: Function;
 
 	handlerCallback() {
